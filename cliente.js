7d53f807729aea26b059097978f25aee92c4b602
INSCRIPCION <149618455+INSCRIPCION@users.noreply.github.com>
2023-11-03 13:37:58 -0500

    cliente.js

--- cliente.js
+++ cliente.js
@@ -22,14 +22,24 @@ socket.onerror = function(error) {
 };
 
 // Agrega un controlador de eventos "blur" a cada campo de entrada
-var inputElements = document.querySelectorAll('input[type="text"]');
+var inputElements = document.querySelectorAll('input[type="text"], input[type="email"]');
 inputElements.forEach(function(inputElement, index) {
     inputElement.addEventListener('blur', function(event) {
         enviarMensaje(inputElement, index);
     });
 });
 
+// Agrega un controlador de eventos "change" a los campos de opción de radio
+var radioElements = document.querySelectorAll('input[type="radio"]');
+radioElements.forEach(function(radioElement, index) {
+    radioElement.addEventListener('change', function(event) {
+        enviarMensaje(radioElement, index);
+    });
+});
+
 function enviarMensaje(inputElement, index) {
-    mensajes[index] = inputElement.value;
+    // Verifica el tipo de campo y obtén el valor adecuado
+    var valor = inputElement.type === "radio" ? inputElement.value : inputElement.value;
+    mensajes[index] = valor;
     socket.send(JSON.stringify(mensajes));
 }
