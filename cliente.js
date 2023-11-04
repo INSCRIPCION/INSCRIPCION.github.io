9c55a70d07583b6587ea619876c833c7a146a51d
INSCRIPCION <149618455+INSCRIPCION@users.noreply.github.com>
2023-11-04 13:01:09 -0500

    cliente.js

--- cliente.js
+++ cliente.js
@@ -1,5 +1,5 @@
 var socket = new WebSocket("ws://localhost:8770");
-var mensajes = [];
+var mensajes = {};
 
 socket.onopen = function(event) {
     console.log("Conexión WebSocket abierta");
@@ -21,23 +21,37 @@ socket.onerror = function(error) {
     console.error("Error en la conexión WebSocket: " + error.message);
 };
 
-// Agregar un controlador de eventos "blur" a los campos de texto y correo electrónico
+// Objeto para mantener un seguimiento de los valores por ID
+var valoresPorID = {};
+
+// Agregar un controlador de eventos "input" a los campos de texto y correo electrónico
 var textAndEmailElements = document.querySelectorAll('input[type="text"], input[type="email"]');
-textAndEmailElements.forEach(function(inputElement, index) {
-    inputElement.addEventListener('blur', function(event) {
-        enviarMensaje(inputElement, index);
+textAndEmailElements.forEach(function(inputElement) {
+    inputElement.addEventListener('input', function(event) {
+        enviarMensaje(inputElement);
     });
 });
 
 // Agregar un controlador de eventos "change" a los campos de opción de radio
 var radioElements = document.querySelectorAll('input[type="radio"]');
-radioElements.forEach(function(radioElement, index) {
+radioElements.forEach(function(radioElement) {
     radioElement.addEventListener('change', function(event) {
-        enviarMensaje(radioElement, index);
+        enviarMensaje(radioElement);
     });
 });
 
-function enviarMensaje(inputElement, index) {
-    mensajes[index] = inputElement.value;
-    socket.send(JSON.stringify(mensajes));
+function enviarMensaje(inputElement) {
+    // Almacena el valor en el objeto valoresPorID usando el ID como clave
+    valoresPorID[inputElement.id] = inputElement.value;
+
+    // Crea un arreglo de objetos que contienen el ID y el valor
+    var valoresConID = Object.keys(valoresPorID).map(function(id) {
+        return {
+            id: id,
+            valor: valoresPorID[id]
+        };
+    });
+
+    // Envía el arreglo de objetos al servidor como mensaje WebSocket
+    socket.send(JSON.stringify(valoresConID));
 }
