4b372b864efd1142dcef2eb627e000902796b0f4
INSCRIPCION <149618455+INSCRIPCION@users.noreply.github.com>
2023-11-04 16:59:16 -0500

    envio.js

--- envio.js
+++ envio.js
@@ -1,5 +1,4 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwbbWTnN8L7ereBjqdXT9Aga-M-BvEwiw09f8gTnfVxx9O2YxEUTMaupC3-BiwApWk3tg/exec'
-const scripturl = 'https://script.google.com/macros/s/AKfycbxVaZsRatyAE1fl5_fNCf6uclhP2wg2tArgqcaJkJdYIPL7A2vaN0Ihz_nfpMOcrPPRKw/exec'
 
 const form = document.forms['contact-form']
 
@@ -9,21 +8,4 @@ form.addEventListener('submit', e => {
   .then(response => alert("Thank you! your form is submitted successfully." ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
-  fetch(scripturl, {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          functionName: 'encontrarNumeroMayor' // Nombre de la función que deseas llamar
-        })
-      })
-        .then(response => response.json())
-        .then(data => {
-          // Manejar la respuesta del script
-          console.log(data);
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
 })
