e84611129d5bcb9e8976bc332995e131eb3f808b
INSCRIPCION <149618455+INSCRIPCION@users.noreply.github.com>
2023-11-04 22:52:55 -0500

    envio.js

--- envio.js
+++ envio.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwSPagtdsZRJWkLZwwkJAiHmrk41mjEe3wWrBncFoVImE3qLXoP41VF9iGfIY57el6mYg/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwLv9oW0wMx0VGgAiGa9kmrsveub_NSz2PG7YAwx7rHFhP4cd1ComkSQ60hpOCtJhSLcQ/exec'
 
 const form = document.forms['contact-form']
 
