04f2061419e046e879c1d39b34f6412840daf309
INSCRIPCION <149618455+INSCRIPCION@users.noreply.github.com>
2023-11-06 09:54:26 -0500

    envio.js

--- envio.js
+++ envio.js
@@ -5,7 +5,7 @@ const form = document.forms['contact-form']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(response => alert("Se ha inscrito satisfactoriamente." ))
   .then(() => { window.location.href = 'index.html'; })
   .catch(error => console.error('Error!', error.message))
 })
